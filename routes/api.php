714652b5591173273c7bd5731099958d87cc0b66
reza <ar.rahmatyy@gmail.com>
2025-03-18 08:13:05 -0700

    add auth middeware

--- routes/api.php
+++ routes/api.php
@@ -10,59 +10,74 @@ use App\Http\Controllers\Api\Admin\SkillController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\Auth\LoginController;
 
-Route::get('/users' , [\App\Http\Controllers\UserController::class , "index"]);
+Route::get('/users', [\App\Http\Controllers\UserController::class, "index"]);
 
-Route::prefix('/auth')->group(function (){
-    Route::post('login' , [LoginController::class , 'loginStore']);
+Route::prefix('/auth')->group(function () {
+    Route::post('login', [LoginController::class, 'loginStore']);
 });
 
-Route::prefix('/services')->group(function (){
+Route::prefix('/services')->group(function () {
     Route::get('/', [ServiceController::class, 'index']);
     Route::get('/show/{service}', [ServiceController::class, 'show']);
-    Route::post('/store', [ServiceController::class, 'store']);
-    Route::put('/update/{service}', [ServiceController::class, 'update']);
-    Route::delete('/destroy/{service}', [ServiceController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [ServiceController::class, 'store']);
+        Route::put('/update/{service}', [ServiceController::class, 'update']);
+        Route::delete('/destroy/{service}', [ServiceController::class, 'destroy']);
+    });
 });
 
-Route::prefix('/skills')->group(function (){
+Route::prefix('/skills')->group(function () {
     Route::get('/', [SkillController::class, 'index']);
     Route::get('/show/{skill}', [SkillController::class, 'show']);
-    Route::post('/store', [SkillController::class, 'store']);
-    Route::put('/update/{skill}', [SkillController::class, 'update']);
-    Route::delete('/destroy/{skill}', [SkillController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [SkillController::class, 'store']);
+        Route::put('/update/{skill}', [SkillController::class, 'update']);
+        Route::delete('/destroy/{skill}', [SkillController::class, 'destroy']);
+    });
 });
 
 
-Route::prefix('/eduction')->group(function (){
+Route::prefix('/eduction')->group(function () {
     Route::get('/', [EductionController::class, 'index']);
     Route::get('/show/{eduction}', [EductionController::class, 'show']);
-    Route::post('/store', [EductionController::class, 'store']);
-    Route::put('/update/{eduction}', [EductionController::class, 'update']);
-    Route::delete('/destroy/{eduction}', [EductionController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [EductionController::class, 'store']);
+        Route::put('/update/{eduction}', [EductionController::class, 'update']);
+        Route::delete('/destroy/{eduction}', [EductionController::class, 'destroy']);
+    });
 });
 
 
 
-Route::prefix('/user-job')->group(function (){
+Route::prefix('/user-job')->group(function () {
     Route::get('/', [UserJobController::class, 'index']);
     Route::get('/show/{userJob}', [UserJobController::class, 'show']);
-    Route::post('/store', [UserJobController::class, 'store']);
-    Route::put('/update/{userJob}', [UserJobController::class, 'update']);
-    Route::delete('/destroy/{userJob}', [UserJobController::class, 'destroy']);
+
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [UserJobController::class, 'store']);
+        Route::put('/update/{userJob}', [UserJobController::class, 'update']);
+        Route::delete('/destroy/{userJob}', [UserJobController::class, 'destroy']);
+    });
 });
 
-Route::prefix('/portfolio')->group(function (){
+Route::prefix('/portfolio')->group(function () {
     Route::get('/', [PortfolioController::class, 'index']);
     Route::get('/show/{portfolio}', [PortfolioController::class, 'show']);
-    Route::post('/store', [PortfolioController::class, 'store']);
-    Route::put('/update/{portfolio}', [PortfolioController::class, 'update']);
-    Route::delete('/destroy/{portfolio}', [PortfolioController::class, 'destroy']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [PortfolioController::class, 'store']);
+        Route::put('/update/{portfolio}', [PortfolioController::class, 'update']);
+        Route::delete('/destroy/{portfolio}', [PortfolioController::class, 'destroy']);
+    });
 });
 
-Route::prefix('/contacts')->group(function (){
+Route::prefix('/contacts')->group(function () {
     Route::get('/', [ContactController::class, 'index']);
     Route::get('/show/{contact}', [ContactController::class, 'show']);
-    Route::post('/store', [ContactController::class, 'store']);
-    Route::put('/update/{contact}', [ContactController::class, 'update']);
-    Route::delete('/destroy/{contact}', [ContactController::class, 'destroy']);
+
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/store', [ContactController::class, 'store']);
+        Route::put('/update/{contact}', [ContactController::class, 'update']);
+        Route::delete('/destroy/{contact}', [ContactController::class, 'destroy']);
+    });
 });
